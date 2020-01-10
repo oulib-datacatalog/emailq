7091a2dc0091e67bab5f94e67dd5991ece78e8e2
Tyler Pearson <tdpearson@ou.edu>
2020-01-10 14:47:26 -0600

    test sendmail

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,8 +1,20 @@
 import sys
-from nose.tools import assert_true, assert_false, assert_equal, nottest
+from nose.tools import assert_true, assert_false, assert_equal, assert_raises, nottest
 try:
     from unittest.mock import MagicMock, Mock, patch
 except ImportError:
     from mock import MagicMock, Mock, patch
 
 from emailq.tasks.tasks import sendmail
+import smtplib
+from celery.exceptions import MaxRetriesExceededError
+
+
+@patch('emailq.tasks.tasks.smtplib.SMTP')
+def test_sendmail(mock_SMTP):
+    to = 'test@test.com'
+    subject = 'test'
+    body = 'test'
+    mock_SMTP.return_value = Mock()
+    assert_true(sendmail(to=to,subject=subject,body=body))
+
